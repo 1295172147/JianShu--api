166ebdfb2272fe65911b4beaf9052f402b88dc0c
weiyuhuanjian <31916597+1295172147@users.noreply.github.com>
2018-10-22 18:15:23 +0800

    add

---
+++ jianshu-api/src/main/java/com/soft1611/jianshu/utils/ResponseUtil.java
@@ -0,0 +1,27 @@
+package com.soft1611.jianshu.utils;
+
+import lombok.Data;
+
+/**
+ * @author asus
+ */
+@Data
+public class ResponseUtil {
+    private int code;
+    private String msg;
+    private Object data;
+
+    public ResponseUtil(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public ResponseUtil(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public ResponseUtil() {
+    }
+}
